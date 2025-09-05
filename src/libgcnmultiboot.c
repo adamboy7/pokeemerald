24b8530fda15cd574e9dfa053d69b344f1ba384e
adamboy7 <adamboy8888@gmail.com>
2025-09-05 12:14:38 -0400

    Add desktop stubs for BIOS and multiboot asm

---
+++ src/libgcnmultiboot.c
@@ -0,0 +1,46 @@
+#include "libgcnmultiboot.h"
+#include "gba/gba.h"
+
+#ifndef GBA
+
+// Desktop stubs for GameCube multiboot routines.
+// TODO: Provide high-level implementation of GameCube link transfers.
+
+u32 GameCubeMultiBoot_Hash(u32 value, u32 key)
+{
+    (void)value;
+    (void)key;
+    // TODO: Implement hashing algorithm used during multiboot.
+    return 0;
+}
+
+void GameCubeMultiBoot_Main(struct GcmbStruct *pStruct)
+{
+    (void)pStruct;
+    // TODO: Drive multiboot transfer state machine.
+}
+
+void GameCubeMultiBoot_ExecuteProgram(struct GcmbStruct *pStruct)
+{
+    (void)pStruct;
+    // TODO: Jump to received multiboot image.
+}
+
+void GameCubeMultiBoot_Init(struct GcmbStruct *pStruct)
+{
+    (void)pStruct;
+    // TODO: Initialise multiboot state before transfer.
+}
+
+void GameCubeMultiBoot_HandleSerialInterrupt(struct GcmbStruct *pStruct)
+{
+    (void)pStruct;
+    // TODO: Handle JOY Bus serial interrupts.
+}
+
+void GameCubeMultiBoot_Quit(void)
+{
+    // TODO: Clean up multiboot session and restore state.
+}
+
+#endif // GBA
